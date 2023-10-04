b631a4b706345cf424c6ed4eceef984b824040e3
vignesh <118158912+viki-kali@users.noreply.github.com>
2023-10-04 15:10:10 +0530

    Create Classroom.java

---
+++ Classroom.java
@@ -0,0 +1,11 @@
+class Classroom {
+    private String className;
+
+    public Classroom(String className) {
+        this.className = className;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+}
