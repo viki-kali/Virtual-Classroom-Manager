d596dd24f3f1dd4ee359549550b728bdeea2c55b
vignesh <118158912+viki-kali@users.noreply.github.com>
2023-10-04 15:12:09 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,17 @@
+class Student {
+    private String studentId;
+    private String className;
+
+    public Student(String studentId, String className) {
+        this.studentId = studentId;
+        this.className = className;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+}
