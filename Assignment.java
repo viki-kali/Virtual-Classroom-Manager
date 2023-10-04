a46f240ba86d01ea1ae705ffcb692ff1455c63e1
vignesh <118158912+viki-kali@users.noreply.github.com>
2023-10-04 15:09:23 +0530

    Create Assignment.java

---
+++ Assignment.java
@@ -0,0 +1,14 @@
+class Assignment {
+    private String className;
+    private String assignmentDetails;
+
+    public Assignment(String className, String assignmentDetails) {
+        this.className = className;
+        this.assignmentDetails = assignmentDetails;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+}
